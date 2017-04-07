58d77edf97c67f0bbd3f60e1ea6280758ac356c4
John Wilson <thawrr@gmail.com>
2017-04-07 14:04:06 -0400

    validations, DB tables, stored procedure

--- GlobalMethods/GMethods.cs
+++ GlobalMethods/GMethods.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,21 @@ namespace GlobalMethods
 {
     public class GMethods
     {
+        public DataSet GetUserNames()//get everything from Users Table
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
 
-    }
+            objCommand.Parameters.Clear();//use this before executing a stored procedure
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "GetAllUsers"; // name of the Stored Procedure
+
+            objDB.GetConnection();
+            DataSet ds = objDB.GetDataSetUsingCmdObj(objCommand);
+            objDB.CloseConnection();
+            
+            return ds;
+        }
+
+    }//end class
 }
