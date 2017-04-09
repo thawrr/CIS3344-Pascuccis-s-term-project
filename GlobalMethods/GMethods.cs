b97c98c533d8e724d2f245c0ef8df7ad5a3c578e
Parth Patel <parthdpatel93@gmail.com>
2017-04-09 10:07:16 -0400

    Revert "validations, DB tables, stored procedure"
    This reverts commit 58d77edf97c67f0bbd3f60e1ea6280758ac356c4.

--- GlobalMethods/GMethods.cs
+++ GlobalMethods/GMethods.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +9,6 @@ namespace GlobalMethods
 {
     public class GMethods
     {
-        public DataSet GetUserNames()//get everything from Users Table
-        {
-            DBConnect objDB = new DBConnect();
-            SqlCommand objCommand = new SqlCommand();
 
-            objCommand.Parameters.Clear();//use this before executing a stored procedure
-            objCommand.CommandType = CommandType.StoredProcedure;
-            objCommand.CommandText = "GetAllUsers"; // name of the Stored Procedure
-
-            objDB.GetConnection();
-            DataSet ds = objDB.GetDataSetUsingCmdObj(objCommand);
-            objDB.CloseConnection();
-            
-            return ds;
-        }
-
-    }//end class
+    }
 }
--- TermProject/Login.aspx.cs
+++ TermProject/Login.aspx.cs
@@ -4,17 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Text;
-using System.Text.RegularExpressions;
-using GlobalMethods;
-using System.Data;
 
 namespace TermProject
 {
     public partial class LogInPage : System.Web.UI.Page
     {
-        Gmethods g = new Gmethods();//object of methods class, mostly calls stored procedures
-
         protected void Page_Load(object sender, EventArgs e)
         {
             //necessary for asp validators... or else
@@ -52,9 +46,7 @@ namespace TermProject
         {
             if (txtEmail.Text == "" || txtEmail.Text.Contains('@') == false)
                 return false;
-            else if (txtPassword.Text == "" || Regex.IsMatch(txtPassword.Text, @"^[a-zA-Z]+[^a-zA-Z]+[^0-9]+$") == false) 
-                // test for empty, then start with character, 
-                //has strings, has ints, and Match zero or one occurrence of the dollar sign
+            else if (txtPassword.Text == "")        // Needs stronger password validation for registration
                 return false;
 
             return true;
@@ -63,8 +55,6 @@ namespace TermProject
         // Check with database to see if credentials are correct
         public bool FoundUser()
         {
-            DataSet ds = g.GetUserNames();
-
             return true;
         }
 
